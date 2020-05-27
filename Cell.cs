02886470498b33b708fae042154fe5076ae3242e
zhu <zhuy1@126.com>
2020-05-27 11:51:45 +0800

    添加项目文件。

---
+++ Cell.cs
@@ -0,0 +1,31 @@
+﻿namespace SnakeGame
+{
+    public class Cell
+    {
+        int indexX;
+        int indexY;
+        int indexOrder;
+
+        Cell()
+        {
+            IndexX = 0;
+            IndexY = 0;
+        }
+
+        public int IndexX { get => indexX; set => indexX = value; }
+        public int IndexY { get => indexY; set => indexY = value; }
+        public int IndexOrder { get => indexOrder; set => indexOrder = value; }
+
+        public bool equals(Cell cell)
+        {
+            if (this.IndexX == cell.IndexX && this.IndexY == cell.IndexY)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Food.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    public class Food
+    {
+        Random random = new Random();
+        int len;
+        Cell[] foodCell = new Cell[20];
+
+        Food()
+        {
+            len = 1;
+            for (int i = 0; i < len; i++)
+            {
+                foodCell[i] = new Cell();
+
+                foodCell[i].setIndexX(setRandomIndexX());
+                foodCell[i].setIndexY(setRandomIndexY());
+            }
+        }
+
+        public static int setRandomIndexX()
+        {
+            Random r = new Random();
+            return (r.nextInt(33) + 1) * 25;
+        }
+
+        public static int setRandomIndexY()
+        {
+            Random r = new Random();
+            return (r.nextInt(24) + 1) * 25;
+        }
+    }
+}
